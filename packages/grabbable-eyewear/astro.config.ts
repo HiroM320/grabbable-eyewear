6126febc4bf9283b89b678ef75fe65eb5d19399e
HiroM320 <56062913+HiroM320@users.noreply.github.com>
2025-04-27 22:23:55 +0900

    GitHub Pages の base が期待通りにならないため調整

--- packages/grabbable-eyewear/astro.config.ts
+++ packages/grabbable-eyewear/astro.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+const repo = 'vrc-items-doc' as const;
+const project = 'grabbable-eyewear' as const;
+const base = `${repo}/${project}/` as const;
+const github_link = `https://github.com/HiroM320/${repo}/tree/main/packages/${project}` as const;
+
 export default defineConfig({
   root: '.',
-  base: '/vrc-items-doc/grabbable-eyewear/',
+  base: base,
   trailingSlash: 'always',
   site: 'https://hirom320.github.io',
   integrations: [
     starlight({
       title: 'Grabbable Eyewear',
-      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/HiroM320/shop-doc/tree/main/packages/grabbable-eyewear' }],
+      social: [{ icon: 'github', label: 'GitHub', href: github_link }],
       sidebar: [
         {slug: 'installation'},
         {slug: 'feature'},
